795d0e50e9c8db432be03801aafb098ed320d01c
Tuomas Eero <eerotuo@gmail.com>
2016-10-25 18:25:31 +0300

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BallController : MonoBehaviour {
+
+    public float initialThrust;
+    public float unacceptableAngle;
+    float angleOfMovement;
+    Rigidbody2D rb;
+    Vector2 initialDir;
+    Vector3 oldVelocity;
+
+
+
+
+    void Start() {
+        initialDir = Vector2.up;
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+        rb.AddForce(initialDir * initialThrust);
+
+    }
+
+    void Update() {
+       
+    }
+
+    void FixedUpdate() {
+        print(rb.velocity);
+        Debug.DrawLine(Vector2.right, rb.velocity);
+
+        oldVelocity = rb.velocity;
+    }
+
+
+
+
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        angleOfMovement = Vector2.Angle(rb.velocity, Vector2.right);
+
+        if (angleOfMovement < unacceptableAngle) {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+    public float paddleSpeed;
+
+
+
+    void FixedUpdate() {
+        Controls();
+    }
+
+
+
+
+
+    void Controls() {
+        if (Input.GetKey("left")) {
+            transform.position += new Vector3(-1, 0, 0) * paddleSpeed;
+        }
+        if (Input.GetKey("right")) {
+            transform.position += new Vector3(1, 0, 0) * paddleSpeed;
+        }
+    }
+}
